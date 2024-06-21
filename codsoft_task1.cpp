2d3ba6fed3ace380743ce23421461480cafc1a9b
Mayank471 <mayankdahiya8431@gmail.com>
2024-06-21 21:36:40 +0530

    Update codsoft_task1.cpp

--- codsoft_task1.cpp
+++ codsoft_task1.cpp
@@ -7,18 +7,18 @@ using namespace std;
 int main()
 {
     srand(time(nullptr)); //To generate new random number everytime//
-    int user_guess=0 , random_number;
-    random_number = rand() % 100 + 1;//To generate the number only between 1 and 100//
+    int user_guess=0 , random_num;
+    random_num = rand() % 100 + 1;//To generate the number only between 1 and 100//
     cout <<"Let's play a game of guessing random numbers." << endl << endl;
     cout <<"Select a random number." << endl;
     while(true)
     {
         cin >>user_guess;
-        if(user_guess>random_number)
+        if(user_guess>random_num)
         {
             cout <<"Guess a lower number" << endl;
         }
-        else if(user_guess<random_number)
+        else if(user_guess<random_num)
         {
             cout <<"Guess a higher number" << endl;
         }
